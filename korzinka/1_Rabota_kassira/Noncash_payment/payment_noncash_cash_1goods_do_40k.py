d99751141a9c29f30dfdec104d6be6be9029c699
accgop <accgop@gmail.com>
2022-12-16 18:15:58 +0500

    Проверка статуса кассы "Касса свободна" и сдачи. Оплата безналом + нал. 1 товар до 40к

---
+++ korzinka/1_Rabota_kassira/Noncash_payment/payment_noncash_cash_1goods_do_40k.py
@@ -0,0 +1,71 @@
+import pyautogui
+from pywinauto.application import Application
+import keyboard
+
+def test_payment_noncash_cash_1goods_do_40k():
+    try:
+        #Старт и соединение с программой
+        app = Application(backend='uia').start(r'cmd.exe /c C:\Users\a.liskin\Desktop\classic.bat',
+            create_new_console=True,
+            wait_for_idle=False).connect(title='MainWindow', timeout=10)
+
+        # Вход в меню кассира
+        app.MainWindow.child_window(title="1. Работа кассира", auto_id="btnKassa", control_type="Button").wrapper_object()
+        keyboard.send('1')
+
+        # Ввод пароля кассира
+        app.InputPassword.child_window(auto_id="PasswordBox", control_type="Edit").wrapper_object()
+        keyboard.send(['6', '5', '4', '3', '2', '1'])
+        keyboard.send('enter')
+
+        # Ввод кода товара с клавиатуры
+        app.Kassir.child_window(auto_id='labelInput', control_type='Text').wrapper_object()
+        pyautogui.press(['4','7', '8', '0', '0', '7', '7', '6', '1', '0', '0', '3', '1'])
+        keyboard.send('enter')
+        app.Kassir.wait('ready', timeout=1)
+
+        # Оплата товара наличными
+        keyboard.send('space')
+        pyautogui.press(['3', '0', '0', '0', '0'])
+        keyboard.send('enter')
+
+        # Выбор варианты оплаты(безналичный расчет)
+        app.CashlessWindow.wait('ready')
+        keyboard.send('tab')
+        keyboard.send('enter')
+
+        # Обращение к полю статуса кассы
+        lbl_ChequeStatus = app.Kassir.child_window(auto_id="lbl_ChequeStatus", control_type="Text").wrapper_object()
+
+        # Проверка статуса кассы
+        assert lbl_ChequeStatus.element_info.rich_text == "ОПЛАТА"
+
+        # Проверка остатка оплаты:
+        labelTextItogo = app.Kassir.child_window(auto_id="labelTextItogo", control_type="Text").wrapper_object()
+        assert labelTextItogo.element_info.rich_text == "ИТОГО: "
+
+        lblItogo = app.Kassir.child_window(auto_id="lblItogo", control_type="Text").wrapper_object()
+        assert lblItogo.element_info.rich_text == "990,00"
+
+        # Ввод оставшейся суммы
+        pyautogui.press(['1', '0', '0', '0'])
+        keyboard.send('enter')
+
+        # Выбор варианты оплаты(наличный расчет)
+        keyboard.send('enter')
+
+        # Проверка статуса кассы
+        lbl_ChequeStatus = app.Kassir.child_window(auto_id="lbl_ChequeStatus", control_type="Text").wrapper_object()
+        assert lbl_ChequeStatus.element_info.rich_text == "КАССА СВОБОДНА"
+
+        # Проверка остатка оплаты:
+        labelTextItogo = app.Kassir.child_window(auto_id="labelTextItogo", control_type="Text").wrapper_object()
+        assert labelTextItogo.element_info.rich_text == "СДАЧА: "
+
+        lblItogo = app.Kassir.child_window(auto_id="lblItogo", control_type="Text").wrapper_object()
+        assert lblItogo.element_info.rich_text == "10,00"
+
+    finally:
+        # Закрытие приложения
+        app = Application().connect(title='MainWindow', timeout=1)
+        app.kill()
